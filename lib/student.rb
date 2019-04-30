72dd9a8b0c78dde3598aa18e4b1210f2a330024b
lieby weintraub <liebahinda@gmail.com>
2019-04-30 06:13:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,6 @@
+require "pry"
+
+
 class Student
   attr_accessor :id, :name, :grade
 
@@ -35,6 +38,18 @@ class Student
       self.new_from_db(row)
     end.first
   
+  end
+   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+        binding.pry
+      end
+      
   end
   
   def save
