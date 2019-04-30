4ae6a593a9e151b9f5d7ce3beab9d182b72a4690
lieby weintraub <liebahinda@gmail.com>
2019-04-30 06:19:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,9 @@ class Student
    SELECT * FROM students 
    WHERE grade IS NOT 12 
    SQL
-   
+   DB[:conn].execute(sql).map do |row|
+     self.new_from_db(row)
+   end 
  end 
   
   def save
