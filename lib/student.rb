f28b6958717f181877500698065b9c1f03e9fb28
lieby weintraub <liebahinda@gmail.com>
2019-04-30 06:24:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,14 @@ class Student
      self.new_from_db(row)
    end 
  end 
+ def self.first_X_students_in_grade_10(num)
+   sql=  <<-SQL
+   SELECT * FROM students 
+   WHERE grade= 10 
+   LIMIT ?
+   SQL
+   
+   
   
   def save
     sql = <<-SQL
